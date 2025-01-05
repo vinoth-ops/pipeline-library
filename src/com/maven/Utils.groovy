b3a38aaef6440d24df3cf53b49f88c65098cb650
vinoth-ops <60887738+vinoth-ops@users.noreply.github.com>
2025-01-05 17:17:52 +0530

    Update Utils.groovy

--- src/com/maven/Utils.groovy
+++ src/com/maven/Utils.groovy
@@ -2,6 +2,7 @@ package com.maven
 
 class Utils {
   public static void getcurrentdir() {
+    echo "message from utils.getcurrentdir: entering method execution"
     println "message from utils.getcurrentdir: entering method execution"
     println "Current working directory: ${System.getProperty('user.dir')}"
   }
