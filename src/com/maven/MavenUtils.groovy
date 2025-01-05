a5e7b96edaa79698eb3d002decfd749cff2199ef
vinoth-ops <60887738+vinoth-ops@users.noreply.github.com>
2025-01-05 14:48:42 +0530

    Update MavenUtils.groovy

--- src/com/maven/MavenUtils.groovy
+++ src/com/maven/MavenUtils.groovy
@@ -5,14 +5,19 @@ class MavenUtils {
     // Method to extract artifactId from pom.xml
     static String getArtifactIdFromPom() {
         try {
-            def artifactId = "grep -oPm1 '(?<=<artifactId>)[^<]+' pom.xml".execute().text.trim()
+            echo "executing getArtifactIdFromPom method"
+
+            sh 'ls -l'  // List files in the current directory
+            
+            def artifactId = sh(script: "grep -oPm1 '(?<=<artifactId>)[^<]+' pom.xml", returnStdout: true).trim()
+            
             if (artifactId.isEmpty()) {
-                println "No artifactId found in pom.xml"
+                echo "No artifactId found in pom.xml"
                 return null
             }
             return artifactId
         } catch (Exception e) {
-            println "Error extracting artifactId: ${e.message}"
+            echo "Error extracting artifactId: ${e.message}"
             return null
         }
     }
@@ -20,21 +25,22 @@ class MavenUtils {
     // Method to extract version from pom.xml
     static String getVersionFromPom() {
         try {
-            def version = "grep -oPm1 '(?<=<version>)[^<]+' pom.xml".execute().text.trim()
+            echo " executing getVersionFromPom"
+            def artifactId = sh(script: "grep -oPm1 '(?<=<version>)[^<]+' pom.xml", returnStdout: true).trim()
             if (version.isEmpty()) {
-                println "No version found in pom.xml"
+                echo "No version found in pom.xml"
                 return null
             }
             return version
         } catch (Exception e) {
-            println "Error extracting version: ${e.message}"
+            echo "Error extracting version: ${e.message}"
             return null
         }
     }
 
     // Method to execute shell commands and return exit status
     static int executeCommand(String command) {
-        println "Executing: $command"
+        echo "Executing: $command"
         def process = command.execute()
         process.waitFor()
         return process.exitValue()
