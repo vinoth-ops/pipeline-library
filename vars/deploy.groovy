6e9320c1e558a551eca5c8d08c860e1967f0993f
vinoth-ops <60887738+vinoth-ops@users.noreply.github.com>
2025-01-05 17:26:47 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -4,6 +4,8 @@ import com.maven.Utils
 
 def call() {
     echo 'message from Deploy.groovy: Deploying the application...'
+
+    echo "Utils class: ${Utils}"
     // Add any other deploy steps, such as running scripts or commands
     Utils.getcurrentdir()
 }
