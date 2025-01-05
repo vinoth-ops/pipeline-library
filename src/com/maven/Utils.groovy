eae88657d68de2c725548a2b262b3c93477f188a
vinoth-ops <60887738+vinoth-ops@users.noreply.github.com>
2025-01-05 23:50:44 +0530

    Update Utils.groovy

--- src/com/maven/Utils.groovy
+++ src/com/maven/Utils.groovy
@@ -4,7 +4,9 @@ class Utils {
     public static String getcurrentdir(script) {
         try {
             script.echo "message from Utils.getcurrentdir: entering method execution"
-            return "Method successfully executed"
+            String cwd = script.pwd()
+            script.echo "Current working directory: ${cwd}"
+            return cwd
         } catch (Exception e) {
             println "Exception: ${e.message}"
             return null
