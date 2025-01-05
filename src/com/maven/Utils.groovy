8e7b06f0668418b08f17be864ac1cdde6fe2194f
vinoth-ops <60887738+vinoth-ops@users.noreply.github.com>
2025-01-05 23:06:28 +0530

    Update Utils.groovy

--- src/com/maven/Utils.groovy
+++ src/com/maven/Utils.groovy
@@ -3,7 +3,6 @@ package com.maven
 class Utils {
   public static String getcurrentdir() {
    try {
-        echo "message from Utils.getcurrentdir: entering method execution"
         
         return "Method successfully executed"
     } catch (Exception e) {
