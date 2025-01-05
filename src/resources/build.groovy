640e3e2c5aad2109054c5b3e1ad8017784680533
vinoth-ops <60887738+vinoth-ops@users.noreply.github.com>
2025-01-05 16:51:46 +0530

    Delete src/resources directory

--- src/resources/build.groovy
+++
@@ -1,6 +0,0 @@
-// vars/build.groovy
-def call() {
-    echo 'Building the application...'
-    // Add any other build steps, such as running scripts or commands
-}
-
--- src/resources/deploy.groovy
+++
@@ -1,6 +0,0 @@
-// vars/deploy.groovy
-def call() {
-    echo 'Deploying the application...'
-    // Add any other deploy steps, such as running scripts or commands
-}
-
